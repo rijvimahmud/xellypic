ae2651f3ef17ec5a2ae057105f920c3dfb624db0
rijvimahmud <rijvimahmud@hotmail.com>
2019-04-07 08:20:28 +0600

    secoend commit

---
+++ python/tuto.py
@@ -0,0 +1,110 @@
+print(
+    "hello world"
+)
+if 5>2:
+  print("five is greater than two")
+  """this is a docstrings"""
+  print('hello world')
+  print("hello world")
+  print("hello")
+  print("hello world")
+  if 5>2:
+      print("five is greater than two")
+      
+x=5
+y="Cezanne_15"
+print(x)
+print(y)
+x= "awesome"
+print("python is " + x)
+x="Python is "
+y="awesome"
+z= x+y
+print(z)
+x=5
+y=10
+print(x+y)
+print("hello")
+
+x=5
+y=10
+print(x+y)
+print("hello")
+if 5 > 3:
+    print("five is grater than three") 
+    if 5>2:
+        print("five is grater than two")
+        x=1
+        y=2.8
+        z=3j
+        print(type(x))
+        print(type(y))
+        print(type(z))
+        x=(3)
+        y=(5j)
+        z=x+y
+        print(z)
+        x=2
+        y=5
+
+        print(x+y)
+        x= int(2)
+        y= int(2.8)
+        z= int("3")
+        print(x)
+        print(y)
+        print(z)
+        x=float(1)
+        y=float(2.8)
+        z=float(3)
+        w=float(4.3)
+        print(x)
+        print(y)
+        print(z)
+        print(w)
+        x=str(1)
+        y=str(3j)
+        z=str(4.0)
+        print(x)
+        print(y)
+        print(z)
+a="hello world"
+print(a[3])
+b="hello world"
+print(b[3:8])
+#The strip() method removes any whitespace from the beginning or the end:
+a="   hello world    "
+print(a.strip())
+#The len() method returns the length of a string:
+a="hello world"
+print(len(a))
+#The lower() method returns the string in lower case:
+a="HELLO WORLD"
+print(a.lower())
+#The upper() method returns the string in upper case:
+a="hello world"
+print(a.upper())
+#The replace() method replaces a string with another string:
+a="hello world"
+print(a.replace("h" , "J"))
+#The split() method splits the string into substrings if it finds instances of the separator:
+a="hello world"
+b=a.split()
+print(b)
+
+e=2
+r=3
+t= e + r
+print(t)
+#Python Arithmetic Operators
+x=3
+y=6
+z=(y/x)
+print(int(z))
+#Command-line String InputPython allows for command line input.That means we are able to ask the user for input.The following example asks for the user's name, then, by using the input() method, the program prints the name to the screen:
+print('enter your name')
+x=input()
+print("Hello " + x)
+
+
+
