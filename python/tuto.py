810035854d13c602efd72cf4c418ab706926b867
Ransome <ransome@localhost.localdomain>
2019-05-07 02:12:07 +0600

    python 3

--- python/tuto.py
+++ python/tuto.py
@@ -170,11 +170,173 @@ print (x >10 or x<10)
 x=4
 print (not(x > 3 and x<5))
 
+#python lists
 
-#Command-line String InputPython allows for command line input.That means we are able to ask the user for input.The following example asks for the user's name, then, by using the input() method, the program prints the name to the screen:
-print('enter your name')
-x=input()
-print("Hello " + x)
+#list
+thislist = ["apple", "banana", "cherry"]
+print (thislist)
+
+#Access Items
+
+thislist = ["apple", "banana", "cherry"]
+print (thislist[1])
+                        
+#Change Item Value
+thislist = ["apple", "banana", "cherry"]
+thislist[1]= "coconut"
+print (thislist)
+
+#Loop Through a List
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+ print (x)
+ 
+ #Check if Item Exists
+ thislist = ["apple", "banana", "cherry"]
+ if "apple" in thislist:
+     print("yes, 'apple' in this list")
+
+#List Length
+     thislist = ["apple", "banana", "cherry"]
+     print(len(thislist))
+     
+#Add Items
+     thislist = ["apple", "banana", "cherry"]
+     thislist.append ("orange")
+     print (thislist)
+     
+#To add an item at the specified index, use the insert() method:
+     thislist= ["apple", "banana", "cherry"]
+     thislist.insert (1, "orange")
+     print (thislist)
+#Remove Item
+     thislist = ["apple", "banana", "cherry"]
+     thislist.remove ("banana")
+     print (thislist)
+     
+     thislist= ["apple", "banana", "cherry"]
+     thislist.pop()
+     print (thislist)
+     
+     thislist = ["apple", "banana", "cherry"]
+     del thislist [1]
+     print (thislist)
+     
+
+     
+     thislist = ["apple", "banana", "cherry"]
+     thislist.clear()
+     print (thislist)
+     
+#copy a list
+     
+     thislist = ["apple", "banana", "cherry"]
+     mylist = thislist.copy()
+     print (mylist)
+     
+          
+     thislist = ["apple", "banana", "cherry"]
+     mylist = list (thislist)
+     print (mylist)
+     
+#The list() Constructor
+thislist = (("apple", "banana", "cherry"))
+print (thislist)
+
+#Tuple
+
+thistuple = ("apple", "banana", "cherry")
+print (thistuple) 
+
+#Access Tuple Items
+thistuple = ("apple", "banana", "cherry")
+print (thistuple[0])
+
+#Change Tuple Values
+#Once a tuple is created, you cannot change its values. Tuples are unchangeable.
 
 
+#Loop Through a Tuple
 
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+    print(x)
+    
+    
+#Check if Item Exists
+    
+    thistuple = ("apple", "banana", "cherry")
+    if "apple" in thistuple:
+        print ("yes, 'apple' is in this fruit list")
+
+#Tuple Length
+        
+    thistuple = ("apple", "banana", "cherry")
+    print (len(thistuple))
+    
+#Remove Items
+#Note: You cannot remove items in a tuple.
+
+#Tuples are unchangeable, so you cannot remove items from it, but you can delete the tuple completely:
+   # thistuple = ("apple", "banana", "cherry")
+   # del thistuple
+    #print (thistuple)#
+   
+#The tuple() Constructor
+thistuple= tuple(("apple", "banana", "cherry"))
+
+print(thistuple)
+
+#Set
+thisset = {"apple", "banana", "cherry"}
+print (thisset)
+
+
+#Access Items
+
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+    print(x)
+    
+thisset = {"apple", "banana", "cherry"}
+print ("banana" in thisset)
+
+#Add Items
+#To add one item to a set use the add() method.
+
+#To add more than one item to a set use the update() method.
+
+thisset = {"apple", "banana", "cherry"}
+thisset.add ("orange" )
+print (thisset)
+
+thisset = {"apple", "banana", "cherry"}
+thisset.update (["orange", "mango"])
+print (thisset)
+
+#Get the Length of a Set
+
+thisset = {'apple', 'mango', 'cherry', 'banana', 'orange'}
+print (len(thisset))
+
+#Remove Item
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("apple")
+print (thisset)
+
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("cherry")
+print (thisset)
+
+thisset = {"apple", "banana", "cherry"}
+x= thisset.pop()
+print (x)
+print (thisset)
+
+thisset = {"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset)
+
+#The set() Constructor
+thisset = set (("apple", "banana", "cherry"))
+print (thisset)
